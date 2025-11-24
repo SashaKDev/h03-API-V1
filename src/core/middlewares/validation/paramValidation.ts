384044963712e1a265dea4daf17f5efd31cfdfc8
Alexander <sasha.kozel252000@gmail.com>
2025-11-25 00:20:07 +0300

    new commit

--- src/core/middlewares/validation/paramValidation.ts
+++ src/core/middlewares/validation/paramValidation.ts
@@ -3,5 +3,5 @@ import {param} from 'express-validator';
 export const idValidation = param('id')
     .isString()
     .withMessage('Must be string')
-    .isNumeric()
-    .withMessage('id must be a number');
+    .isMongoId()
+    .withMessage('Must be MongoId');
