447d336bee41f433001280c2b08575031358da61
Alexander <sasha.kozel252000@gmail.com>
2025-11-25 00:20:00 +0300

    new commit

--- src/posts/router/handlers/getAllPostsHandler.ts
+++ src/posts/router/handlers/getAllPostsHandler.ts
@@ -1,8 +1,8 @@
 import {Request, Response} from "express";
 import {postsRepository} from "../../repositories/postsRepository";
 
-export const getAllPostsHandler = (req: Request, res: Response) => {
-    const foundPosts = postsRepository.findAll();
+export const getAllPostsHandler = async (req: Request, res: Response) => {
+    const foundPosts = await postsRepository.findAll();
     res
         .status(200)
         .json(foundPosts);
