9a441686184e5eedf919355e09d8ab7deeb27b06
Alexander <sasha.kozel252000@gmail.com>
2025-11-25 00:19:56 +0300

    new commit

--- src/blogs/router/handlers/getAllBlogsHandler.ts
+++ src/blogs/router/handlers/getAllBlogsHandler.ts
@@ -1,9 +1,8 @@
 import {Request, Response} from 'express';
-import {db} from "../../../db/in-memory.db";
 import {blogsRepository} from "../../repositories/blogsRepository";
 
-export const getAllBlogsHandler = (req: Request, res: Response) => {
-    const allBlogs = blogsRepository.findAll()
+export const getAllBlogsHandler = async (req: Request, res: Response) => {
+    const allBlogs = await blogsRepository.findAll()
     res
         .status(200)
         .json(allBlogs);
