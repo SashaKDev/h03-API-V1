0124cbfae3ffc91f8440f9b294ff67405504e06d
Alexander <sasha.kozel252000@gmail.com>
2025-11-25 00:19:50 +0300

    new commit

--- src/blogs/router/handlers/deleteBlogHandler.ts
+++ src/blogs/router/handlers/deleteBlogHandler.ts
@@ -1,12 +1,17 @@
 import {Request, Response} from "express";
 import {blogsRepository} from "../../repositories/blogsRepository";
 
-export const deleteBlogHandler = (req: Request, res: Response) => {
-    const foundBlog = blogsRepository.findById(req.params.id);
-    if (!foundBlog) {
+export const deleteBlogHandler = async (req: Request, res: Response) => {
+    const blog = await blogsRepository.findById(req.params.id);
+    if (!blog) {
         res.sendStatus(404);
-        return;
     }
-    blogsRepository.delete(req.params.id);
+
+    try {
+        await blogsRepository.delete(req.params.id);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+
     res.sendStatus(204);
 }
