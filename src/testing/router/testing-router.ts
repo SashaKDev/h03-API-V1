595ba4cab3f4e4c6422fdc794e9fd3c05c078f03
Alexander <sasha.kozel252000@gmail.com>
2025-11-26 20:19:39 +0300

    new commit

--- src/testing/router/testing-router.ts
+++ src/testing/router/testing-router.ts
@@ -1,11 +1,10 @@
-// import {Router, Request, Response} from "express";
-// // import {db} from "../../db/in-memory.db";
-//
-//
-// export const testingRouter = Router({});
-//
-// testingRouter.delete('/all-data', (req: Request, res: Response) => {
-//     db.blogs = [];
-//     db.posts = [];
-//     res.sendStatus(204);
-// });
+import {Router, Request, Response} from "express";
+import {blogsCollection, postsCollection} from "../../db/mongo.db";
+
+export const testingRouter = Router({});
+
+testingRouter.delete('/all-data', async (req: Request, res: Response) => {
+    await postsCollection.deleteMany();
+    await blogsCollection.deleteMany();
+    res.sendStatus(204);
+})
