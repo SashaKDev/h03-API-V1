0f8492f527f052365fcb9d358916350d9749d0ca
Alexander <sasha.kozel252000@gmail.com>
2025-11-26 20:19:30 +0300

    new commit

--- src/blogs/router/handlers/getAllBlogsHandler.ts
+++ src/blogs/router/handlers/getAllBlogsHandler.ts
@@ -1,9 +1,11 @@
 import {Request, Response} from 'express';
 import {blogsRepository} from "../../repositories/blogsRepository";
+import {mapBlogToViewModel} from "../../mapers/mapBlogToViewModel";
 
 export const getAllBlogsHandler = async (req: Request, res: Response) => {
     const allBlogs = await blogsRepository.findAll()
+    const allBlogsViewModel = allBlogs.map(mapBlogToViewModel);
     res
         .status(200)
-        .json(allBlogs);
+        .json(allBlogsViewModel);
 }
