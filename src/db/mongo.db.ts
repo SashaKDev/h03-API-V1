4bf044b63a0d0ec3c767244f05e81de14f3bdc7c
Alexander <sasha.kozel252000@gmail.com>
2025-11-26 20:19:05 +0300

    new commit

--- src/db/mongo.db.ts
+++ src/db/mongo.db.ts
@@ -1,6 +1,7 @@
 import {Collection, Db, MongoClient} from 'mongodb';
 import {Blog} from "../blogs/types/blog";
 import {Post} from "../posts/types/post";
+import {SETTINGS} from "../core/settings/settings";
 
 export let blogsCollection: Collection<Blog>;
 export let postsCollection: Collection<Post>;
@@ -8,7 +9,7 @@ export let postsCollection: Collection<Post>;
 export const runDb = async (dbUrl: string) => {
 
     const client = new MongoClient(dbUrl);
-    const db: Db = client.db('incubator-db');
+    const db: Db = client.db(SETTINGS.DB);
     blogsCollection = db.collection('blogs');
     postsCollection = db.collection('posts');
 
