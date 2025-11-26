43f6502dc0d342123325ed89012538b2bbc17dbb
Alexander <sasha.kozel252000@gmail.com>
2025-11-26 20:18:57 +0300

    new commit

--- src/posts/repositories/postsRepository.ts
+++ src/posts/repositories/postsRepository.ts
@@ -1,6 +1,6 @@
 import {Post} from "../types/post";
 import {PostInputDto} from "../dto/post-input.dto";
-import {WithId} from "mongodb";
+import {ObjectId, WithId} from "mongodb";
 import {postsCollection} from "../../db/mongo.db";
 
 //
@@ -8,29 +8,27 @@ export const postsRepository = {
     async findAll(): Promise<WithId<Post>[]>{
         return await postsCollection.find().toArray();
     },
-//
-//     findById(id: string): Post | null{
-//         return db.posts.find(post => post.id === id) ?? null
-//     },
-//
+
+    async findById(id: string): Promise<WithId<Post> | null>{
+        return await postsCollection.findOne({_id: new ObjectId(id)});
+    },
+
     async create(post: Post): Promise<WithId<Post>> {
         const insertedPost = await postsCollection.insertOne(post);
         return ({_id: insertedPost.insertedId, ...post});
     },
-//
-//     update(id: string, dto: PostInputDto) {
-//         const foundPost = db.posts.find(post => post.id === id);
-//         const foundBlog = db.blogs.find(blog => blog.id === dto.blogId);
-//         if (foundPost && foundBlog) {
-//             foundPost.title = dto.title;
-//             foundPost.shortDescription = dto.shortDescription;
-//             foundPost.content = dto.content;
-//             foundPost.blogId = dto.blogId;
-//             foundPost.blogName = foundBlog.name;
-//         }
-//     },
-//
-//     delete(id: string) {
-//         db.posts = db.posts.filter(posts => posts.id !== id);
-//     }
+
+    async update(id: string, dto: PostInputDto): Promise<void> {
+        await postsCollection.updateOne({_id: new ObjectId(id)}, {
+            $set: {
+                title: dto.title,
+                shortDescription: dto.shortDescription,
+                content: dto.content,
+                blogId: dto.blogId
+            }});
+    },
+
+    async delete(id: string): Promise<void> {
+        await postsCollection.deleteOne({_id: new ObjectId(id)});
+    }
 };
