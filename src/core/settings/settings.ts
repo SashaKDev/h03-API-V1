31e0d00049286d07070a97a2ddb92bbb2cbc34cb
Alexander <sasha.kozel252000@gmail.com>
2025-11-27 13:19:29 +0300

    new mongo adress

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -2,5 +2,8 @@ export const SETTINGS = {
     USERNAME: 'admin',
     PASSWORD: 'qwerty',
     DB: 'testData',
-    MONGO_URL: 'mongodb://localhost:27017/testdb',
+    //local MongoDB
+    // MONGO_URL: 'mongodb://localhost:27017/testdb',
+    //atlas MongoDB
+    MONGO_URL: 'mongodb+srv://root:12344321@cluster0.gcyvrjh.mongodb.net/?appName=Cluster0'
 }
